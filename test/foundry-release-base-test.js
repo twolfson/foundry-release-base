7713ed7254aa3be53671aeb328e864754fdc2182
Todd Wolfson <todd@twolfson.com>
2015-09-26 15:55:10 -0500

    Added another set of test cases

--- test/foundry-release-base-test.js
+++ test/foundry-release-base-test.js
@@ -3,9 +3,9 @@ var expect = require('chai').expect;
 var childUtils = require('./utils/child');
 
 // Start our tests
-describe('foundry-release-base', function () {
+describe('A full command based on foundry-release-base', function () {
   describe('running --spec-version', function () {
-    childUtils.exec('base-plugin --spec-version');
+    childUtils.exec('full-plugin --spec-version');
 
     it('has no errors', function () {
       expect(this.err).to.equal(null);
@@ -14,6 +14,125 @@ describe('foundry-release-base', function () {
 
     it('returns the spec version', function () {
       expect(this.stdout).to.match(/^\d+\.\d+\.\d+$/);
+    });
+  });
+
+  describe('running update-files', function () {
+    childUtils.exec('full-plugin update-files');
+
+    it('has no errors', function () {
+      expect(this.err).to.equal(null);
+      expect(this.stderr).to.equal('');
+    });
+
+    it('returns the expected text', function () {
+      expect(this.stdout).to.match('Step run: update-files');
+    });
+  });
+
+  describe('running commit', function () {
+    childUtils.exec('full-plugin commit');
+
+    it('has no errors', function () {
+      expect(this.err).to.equal(null);
+      expect(this.stderr).to.equal('');
+    });
+
+    it('returns the expected text', function () {
+      expect(this.stdout).to.match('Step run: commit');
+    });
+  });
+
+  describe('running register', function () {
+    childUtils.exec('full-plugin register');
+
+    it('has no errors', function () {
+      expect(this.err).to.equal(null);
+      expect(this.stderr).to.equal('');
+    });
+
+    it('returns the expected text', function () {
+      expect(this.stdout).to.match('Step run: register');
+    });
+  });
+
+  describe('running publish', function () {
+    childUtils.exec('full-plugin publish');
+
+    it('has no errors', function () {
+      expect(this.err).to.equal(null);
+      expect(this.stderr).to.equal('');
+    });
+
+    it('returns the expected text', function () {
+      expect(this.stdout).to.match('Step run: publish');
+    });
+  });
+});
+
+describe('An empty command based on foundry-release-base', function () {
+  describe('running --spec-version', function () {
+    childUtils.exec('empty-plugin --spec-version');
+
+    it('has no errors', function () {
+      expect(this.err).to.equal(null);
+      expect(this.stderr).to.equal('');
+    });
+
+    it('returns the spec version', function () {
+      expect(this.stdout).to.match(/^\d+\.\d+\.\d+$/);
+    });
+  });
+
+  describe('running update-files', function () {
+    childUtils.exec('empty-plugin update-files');
+
+    it('has no errors', function () {
+      expect(this.err).to.equal(null);
+      expect(this.stderr).to.equal('');
+    });
+
+    it('has no output', function () {
+      expect(this.stdout).to.match('');
+    });
+  });
+
+  describe('running commit', function () {
+    childUtils.exec('empty-plugin commit');
+
+    it('has no errors', function () {
+      expect(this.err).to.equal(null);
+      expect(this.stderr).to.equal('');
+    });
+
+    it('returns the expected text', function () {
+      expect(this.stdout).to.match('');
+    });
+  });
+
+  describe('running register', function () {
+    childUtils.exec('empty-plugin register');
+
+    it('has no errors', function () {
+      expect(this.err).to.equal(null);
+      expect(this.stderr).to.equal('');
+    });
+
+    it('returns the expected text', function () {
+      expect(this.stdout).to.match('');
+    });
+  });
+
+  describe('running publish', function () {
+    childUtils.exec('empty-plugin publish');
+
+    it('has no errors', function () {
+      expect(this.err).to.equal(null);
+      expect(this.stderr).to.equal('');
+    });
+
+    it('returns the expected text', function () {
+      expect(this.stdout).to.match('');
     });
   });
 });
